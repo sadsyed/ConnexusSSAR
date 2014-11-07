db9b4ac3471863fd19667b62cf7517791bf37f03
Amy Reed <amy_hindman@yahoo.com>
2014-11-06 20:50:34 -0600

    Fixed issue from merge with extra entry in manifest.

--- app/src/main/java/ssar/apt/connexusssar/util/ConnexusSSARConstants.java
+++ app/src/main/java/ssar/apt/connexusssar/util/ConnexusSSARConstants.java
@@ -5,8 +5,8 @@ package ssar.apt.connexusssar.util;
  */
 public class ConnexusSSARConstants {
     public static final String CONNEXUSSSAR_DEBUG_TAG = "CONEXUSSSSAR_DEBUG";
+    public static final String APP_SPOT = "http://connexusssar2.appspot.com";
     //public static final String APP_SPOT = "http://sonic-fiber-734.appspot.com";
-    public static final String APP_SPOT = "http://sonic-fiber-734.appspot.com";
     public static final String VIEW_ALL_STREAMS = APP_SPOT + "/ViewAllStreamsService";
     public static final String VIEW_ASTREAM = APP_SPOT + "/ViewStreamService";
     public static final String MANAGE_STREAM = APP_SPOT + "/ManageStreamService";
